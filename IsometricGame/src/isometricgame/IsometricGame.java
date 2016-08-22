0493a52de2e3229e79b5b08e39d77c7f6f84a9a0
hTw-The-Legend <high.tech.war.aut@gmail.com>
2016-08-22 20:20:48 +0200

    Setup Project

---
+++ IsometricGame/src/isometricgame/IsometricGame.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isometricgame;
+
+/**
+ *
+ * @author hTw-The-Legend
+ */
+public class IsometricGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
