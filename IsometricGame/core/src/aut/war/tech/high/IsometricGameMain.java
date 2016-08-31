e6c8837ee8be5ddf76d8fd6f9337d5614d5e8558
hTw-The-Legend <high.tech.war.aut@gmail.com>
2016-08-31 22:55:55 +0200

    Set window name

--- IsometricGame/core/src/aut/war/tech/high/IsometricGameMain.java
+++ IsometricGame/core/src/aut/war/tech/high/IsometricGameMain.java
@@ -14,14 +14,14 @@ public class IsometricGameMain extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-                System.out.println("Hello World!");
-                // Hallo wie gehts
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+                // Window Name
+                Gdx.graphics.setTitle("Game");
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
